f2e9bf1b708720beb73e3fa262d9429f1782a644
Chill <123209872+dimddimich@users.noreply.github.com>
2023-03-26 12:23:57 +0300

    Add files via upload

---
+++ praka5.3/praka5.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace praka5_3
+{
+    interface IFilter
+    {
+        string Execute(string textLine);
+    }
+
+    class DigitFilter : IFilter
+    {
+        public string Execute(string textLine)
+        {
+            return Regex.Replace(textLine, @"\d", "");
+        }
+    }
+
+    class LetterFilter : IFilter
+    {
+        public string Execute(string textLine)
+        {
+            return Regex.Replace(textLine, @"[a-zA-Z]", "");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = "Vladelec VAZ 2115";
+            IFilter digitFilter = new DigitFilter();
+            IFilter letterFilter = new LetterFilter();
+
+            string outputText1 = digitFilter.Execute(inputText);
+            string outputText2 = letterFilter.Execute(inputText);
+
+            Console.WriteLine(outputText1);
+            Console.WriteLine(outputText2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ praka5.4/praka5.4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace praka5_4
+{
+    interface IShare
+    {
+        void Draw(int size);
+    }
+
+    class VerticalLine : IShare
+    {
+        public void Draw(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("|");
+            }
+        }
+    }
+
+    class HorizontalLine : IShare
+    {
+        public void Draw(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Square : IShare
+    {
+        public void Draw(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            IShare verticalLine = new VerticalLine();
+            IShare horizontalLine = new HorizontalLine();
+            IShare square = new Square();
+
+            Console.WriteLine("Вертикальная линия:");
+            verticalLine.Draw(size);
+
+            Console.WriteLine("Горизонтальная линия:");
+            horizontalLine.Draw(size);
+
+            Console.WriteLine("Квадрат:"); //Правда скорее прямоугольник, ну ладно
+            square.Draw(size);
+
+            Console.ReadKey();
+        }
+    }
+}
+
